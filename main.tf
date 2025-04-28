9bf3e5593db8ae3250d727bed1bc79d3f9d26481
Aaron Evans <aarone@outlook.com.au>
2025-04-28 15:24:14 +1000

    Format alignment of VM module parameters for improved readability

--- main.tf
+++ main.tf
@@ -12,17 +12,17 @@ module "single_virtual_machine" {
   source  = "app.terraform.io/tfo-apj-demos/single-virtual-machine/vsphere"
   version = "~> 1.2"
 
-  hostname         = each.value.hostname
-  ad_domain        = each.value.ad_domain
-  backup_policy    = each.value.backup_policy
-  environment      = each.value.environment
-  os_type          = each.value.os_type
+  hostname           = each.value.hostname
+  ad_domain          = each.value.ad_domain
+  backup_policy      = each.value.backup_policy
+  environment        = each.value.environment
+  os_type            = each.value.os_type
   linux_distribution = each.value.linux_distribution
-  security_profile = each.value.security_profile
-  site             = each.value.site
-  size             = each.value.size
-  storage_profile  = each.value.storage_profile
-  tier             = each.value.tier
+  security_profile   = each.value.security_profile
+  site               = each.value.site
+  size               = each.value.size
+  storage_profile    = each.value.storage_profile
+  tier               = each.value.tier
 }
 
 # Create AAP inventory for VMs
