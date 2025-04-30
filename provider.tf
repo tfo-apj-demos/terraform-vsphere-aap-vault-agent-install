291776d226ecaef2cde11d45bf187e65d25b8f9f
Aaron Evans <aarone@outlook.com.au>
2025-04-30 11:34:26 +1000

    Reorder AAP provider configuration for consistency

--- provider.tf
+++ provider.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
-    aap = {
-      source  = "ansible/aap"
-      version = "~> 1"
-    }
     vsphere = {
       source  = "hashicorp/vsphere"
       version = "~> 2"
     }
+    aap = {
+      source  = "ansible/aap"
+      version = "~> 1"
+    }
   }
-}
-
-provider "aap" {
 }
