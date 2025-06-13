04c104a23476bd1037c6df60509ac8d875255233
Aaron Evans <aarone@outlook.com.au>
2025-06-13 14:08:14 +1000

    Refactor output variables in Terraform configuration to use module references for virtual machine name and IP address

--- output.tf
+++ output.tf
@@ -1,34 +1,9 @@
 output "vm_name" {
     description = "Name of the created virtual machine"
-    value       = vsphere_virtual_machine.vm.name
-}
-
-output "vm_id" {
-    description = "ID of the created virtual machine"
-    value       = vsphere_virtual_machine.vm.id
+    value       = module.single_virtual_machine.virtual_machine_name
 }
 
 output "vm_ip_address" {
     description = "IP address of the virtual machine"
-    value       = vsphere_virtual_machine.vm.default_ip_address
-}
-
-output "vm_guest_ip_addresses" {
-    description = "All IP addresses assigned to the virtual machine"
-    value       = vsphere_virtual_machine.vm.guest_ip_addresses
-}
-
-output "vm_uuid" {
-    description = "UUID of the virtual machine"
-    value       = vsphere_virtual_machine.vm.uuid
-}
-
-output "vm_moid" {
-    description = "Managed object ID of the virtual machine"
-    value       = vsphere_virtual_machine.vm.moid
-}
-
-output "vm_fqdn" {
-    description = "Fully qualified domain name of the virtual machine"
-    value       = vsphere_virtual_machine.vm.guest_ip_addresses != null ? "${vsphere_virtual_machine.vm.name}.${var.vm_config.ad_domain}" : null
+    value       = module.single_virtual_machine.ip_address
 }
