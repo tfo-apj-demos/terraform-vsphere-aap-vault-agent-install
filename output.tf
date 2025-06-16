cbe82a0da48efd2a1bb6c8fa555d1289fa2a57be
Aaron Evans <aarone@outlook.com.au>
2025-06-16 14:12:59 +1000

    Comment out output variables for virtual machine name and IP address in Terraform configuration

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "vm_name" {
-    description = "Name of the created virtual machine"
-    value       = module.single_virtual_machine.virtual_machine_name
-}
+# output "vm_name" {
+#     description = "Name of the created virtual machine"
+#     value       = module.single_virtual_machine.virtual_machine_name
+# }
 
-output "vm_ip_address" {
-    description = "IP address of the virtual machine"
-    value       = module.single_virtual_machine.ip_address
-}
+# output "vm_ip_address" {
+#     description = "IP address of the virtual machine"
+#     value       = module.single_virtual_machine.ip_address
+# }
