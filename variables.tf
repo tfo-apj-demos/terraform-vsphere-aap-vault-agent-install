5c5e5a7e9d699551ce3d9dd9e685c4043536094f
Aaron Evans <aarone@outlook.com.au>
2025-06-16 14:21:38 +1000

    Add missing newline at end of file in variables.tf

--- variables.tf
+++ variables.tf
@@ -24,3 +24,13 @@ variable "job_template_id" {
 
 # TFC Workspace ID for inventory naming
 variable "TFC_WORKSPACE_ID" {}
+
+variable "ad_domain_name" {
+  description = "The name of the Active Directory domain."
+  type        = string
+}
+variable "admin_password" {
+  description = "The password for the administrator account."
+  type        = string
+  sensitive   = true
+}
